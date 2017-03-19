6f1a38462e64c4284fb8a93f28315bab4b03c724
cryystalsong <cryystalsong@gmail.com>
2017-03-18 21:38:24 -0700

    INITIAL

---
+++ Save-ty_User/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Save-ty_User
+//
+//  Created by Crystal Song on 2017-03-18.
+//  Copyright (c) 2017 Crystal Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
